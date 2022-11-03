1c0389edba35e4b211f46a9df52a37bbcd9c864e
0dot77 <yty0706@gmail.com>
2022-11-03 11:37:46 +0100

    두 번째 페이지

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,3 +1,4 @@
+import styled from 'styled-components';
 import { Suspense, lazy } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
@@ -9,10 +10,30 @@ const AMnews = lazy(() => import('./pages/desktop/AM/AMnews'));
 
 const RProgram = lazy(() => import('./pages/desktop/RP/RProgram'));
 
+const LoadingContainer = styled.section`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  h1 {
+    color: white;
+    font-size: 1rem;
+    font-style: italic;
+  }
+`;
+
 export default function Router() {
   return (
     <BrowserRouter>
-      <Suspense fallback={<h1>준비중...</h1>}>
+      <Suspense
+        fallback={
+          <LoadingContainer>
+            <h1>기관에 송신 요청 중...</h1>
+          </LoadingContainer>
+        }
+      >
         <Routes>
           <Route
             path="/"
--- src/pages/mobile/HoMobileSecond.jsx
+++ src/pages/mobile/HoMobileSecond.jsx
@@ -1,10 +1,17 @@
 import styled from 'styled-components';
+import NoiseObj from '../../components/NoiseObj';
 
 const SecondLayout = styled.section`
   width: 100%;
   height: 100vh;
+  scroll-snap-align: start;
+  scroll-snap-stop: always;
 `;
 
 export default function HoMobileSecond() {
-  return <></>;
+  return (
+    <SecondLayout>
+      <NoiseObj />
+    </SecondLayout>
+  );
 }
