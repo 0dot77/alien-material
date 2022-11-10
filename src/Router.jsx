1f04766bab4b9fede6505a98f81fd8455ed56d77
0dot77 <yty0706@gmail.com>
2022-11-10 13:57:45 +0900

    test

--- src/Router.jsx
+++ src/Router.jsx
@@ -55,7 +55,7 @@ export default function Router() {
           />
           <Route
             path="/research"
-            element={isMobile ? <RProgramMobile /> : <RProgram />}
+            element={<RProgram />}
           />
         </Routes>
       </Suspense>
