e8b07c2541ae6df119a7e7de9444f516a7101b63
0dot77 <yty0706@gmail.com>
2022-11-10 16:26:31 +0900

    미디어쿼리.. 마지막 수정

--- src/pages/RPMain.jsx
+++ src/pages/RPMain.jsx
@@ -1,17 +1,27 @@
 import { Suspense, lazy } from 'react';
 import { useMediaQuery } from 'react-responsive';
+import styled from 'styled-components';
+import RPLayout from '../components/ForthPages/RPLayout';
+import RPparticipant from '../components/ForthPages/RPparticipant';
 
-const RProgram = lazy(() => import('./desktop/RP/RProgram'));
-const RProgramMobile = lazy(() => import('./mobile/RP/RProgramMobile'));
+const ParticipantContainer = styled.section`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const RPMain = () => {
   const checkMobile = useMediaQuery({
     query: '(max-width:767px)',
   });
   return (
-    <Suspense fallback={<h1>연구 데이터를 불러오는 중 입니다...</h1>}>
-      {checkMobile ? <RProgramMobile /> : <RProgram />}
-    </Suspense>
+    <RPLayout>
+      <ParticipantContainer>
+        <RPparticipant checkDeviceIsMobile={checkMobile} />
+      </ParticipantContainer>
+    </RPLayout>
   );
 };
 
